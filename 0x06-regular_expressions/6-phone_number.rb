25600bede7d4fc3348049aab708fcfb59897e393
RangiraDave <rangiradev666@gmail.com>
2024-01-04 20:42:21 +0200

    Added script for phone number

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
 if ARGV.empty?
-  puts "You must have atleast one argument!"
+  puts "Must put atleast one argment"
   exit(1)
 end
 
 input = ARGV[0]
-pattern = /^h[a-zA-Z0-9]n$/
+pattern = /^[0-9]{10}$/
 matches = input.scan(pattern)
 
 if matches
