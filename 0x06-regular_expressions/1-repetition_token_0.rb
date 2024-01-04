d12dad963126915d297e0637406c167110da685a
RangiraDave <rangiradev666@gmail.com>
2024-01-04 19:41:12 +0200

    removed space in {2,5}

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
+
 if ARGV.empty?
   puts "Must have atleast one argument!"
   exit(1)
 end
+
 input = ARGV[0]
-pattern = /hbt{2, 5}n/
+pattern = /hbt{2,5}n/
 matches = input.scan(pattern)
+
 if matches.empty?
   puts "No matches found."
 else
