77b823960b7adef1a5024aea7a66e8bd69fa6829
RangiraDave <rangiradev666@gmail.com>
2024-02-23 15:01:59 +0200

    Used Perm instead of permission

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Script to create a file
 file {'/tmp/school':
-insure     => present,
-permission => 0744,
+ensure     => present,
+Perm => 0744,
 owner      => www-data,
 group      => www-data,
 content    => 'I love Puppet',
