ceb330df9f0d9fd876dc22360d61a9cd9cad0d77
RangiraDave <rangiradev666@gmail.com>
2024-02-23 15:34:09 +0200

    used mode instead of Perm

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Manifest to create a file
 
 file {'/tmp/school':
-ensure  => present,
-Perm    => 0744,
-owner   => www-data,
-group   => www-data,
+ensure  => 'present',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Manifest to install flask
 
 package {'flask 2.1.0':
-ensure   => installed,
-provider => pip3,
+ensure   => 'installed',
+provider => 'pip3',
 }
