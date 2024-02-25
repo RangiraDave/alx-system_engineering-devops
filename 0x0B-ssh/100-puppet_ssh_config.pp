6bc49a90cc6d95b6fe2fcfb801b9d2d1c4663a4c
RangiraDave <rangiradev666@gmail.com>
2024-02-25 10:48:20 +0200

    configured the client to login without password validation

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,11 @@
 # manisscript to configure my server to not use password.
 
-$str = "PermitRootLogin no
-PasswordAuthentication no"
+$str = "
+PermitRootLogin no
+PasswordAuthentication no
+Include /etc/ssh/sshd_config.d/*.conf
+AcceptEnv LANG LC_*
+"
 
 file {'/etc/ssh/sshd_config':
 ensure  => present,
