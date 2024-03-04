945c1db3f602cca864c8822671ef61cbde38c6c4
RangiraDave <rangiradev666@gmail.com>
2024-03-04 16:15:52 +0200

    corrected availlable to available

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,44 +2,71 @@
 
 # install nginx package
 package { 'nginx':
- ensure => installed,
+  ensure => installed,
 }
 
 # Define the custom index page content
 file { '/var/www/html/index.nginx-debian.html':
- ensure  => present,
- content => 'Hello World!\n',
+  ensure  => present,
+  content => 'Hello World!\n',
 }
 
-# Configure Nginx server block
+# Configure Nginx server block (On availlable)
 file { '/etc/nginx/sites-available/default':
-ensure  => present,
-content => '
- server {
-  listen 80 default_server;
-  listen [::]:80 default_server;
+ensure    => present,
+content   => '
+  server {
+   listen 80 default_server;
+   listen [::]:80 default_server;
 
-  add_header X-Served-By $hostname;
+   add_header X-Served-By $hostname;
 
-  root /var/www/html;
-  index index.html index.htm index.nginx-debian.html;
+   root /var/www/html;
+   index index.html index.htm index.nginx-debian.html;
 
-  location / {
-   return 200 'Hello World!';
+   location / {
+    return 200 'Hello World!';
   }
 
   # 301 Redirect /redirect_me
-  location /redirect_me {
-   return 301 https://www.youtube.com/watch?v=lE-qjOzwk3s;
-  }
- }',
+   location /redirect_me {
+     return 301 https://www.youtube.com/watch?v=lE-qjOzwk3s;
+   }
+  }',
 
- require => Package['nginx'],
- notify  => Service['nginx'],
+# Configure Nginx server block(on enabled)
+file { '/etc/nginx/sites-enabled/default':
+ensure    => present,
+content   => '
+  server {
+   listen 80 default_server;
+   listen [::]:80 default_server;
+
+   add_header X-Served-By $hostname;
+
+   root /var/www/html;
+   index index.html index.htm index.nginx-debian.html;
+
+   location / {
+    return 200 'Hello World!';
+  }
+
+  # 301 Redirect /redirect_me
+   location /redirect_me {
+     return 301 https://www.youtube.com/watch?v=lE-qjOzwk3s;
+   }
+  }',
+
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+  require => Package['nginx'],
+  notify  => Service['nginx'],
 }
 
 # Ensure Nginx service is running and enabled
 service { 'nginx':
- ensure => running,
- enable => true,
+  ensure => running,
+  enable => true,
 }
