e7e54b040d94d64186849023fed7422e27b0fc19
RangiraDave <rangiradev666@gmail.com>
2024-04-09 11:49:55 +0200

    Trying word count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Function to return number of subscribers from the Reddit API. """
+
+import requests
+
+
+def count_words(subreddit, word_list):
+    """ The logic is handled here. """
+
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    header = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        req = requests.get(url, headers=header)
+        req.raise_for_status()
+
+        data = req.json()
+        count = data['data']['subscribers']
+    except Exception:  # requests.HTTPError:
+        count = 0
+
+    return count
