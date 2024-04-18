72a4f173400959bb7fe2301515318bfc15fd79bb
RangiraDave <rangiradev666@gmail.com>
2024-04-18 08:41:29 +0200

    Added the missing comma

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,7 @@
 # Fixed by increasing hard user and soft file limit for user holberton.
 
 exec { 'increase_hard_limit':
-  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf'
+  command => 'sed -i "/holberton hard/s/5/10000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
